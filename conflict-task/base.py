e166ffff534d03172864d0cf5268b2d02ed1ed7a
Haffi921 <haffi921@gmail.com>
2021-07-01 16:11:28 +0200

    Minor adjustments

--- conflict-task/base.py
+++ conflict-task/base.py
@@ -1,9 +1,24 @@
+from subprocess import run
+import psychopy
+
+
 from psychopy import visual, core
+from psychopy.hardware import keyboard
 
-win = visual.Window(fullscr=True)
-msg = visual.TextStim(win, text="Hello, world!")
+win = visual.Window(
+    size=(1920, 1200), fullscr=True, screen=0, 
+    winType='pyglet', allowGUI=False, allowStencil=False,
+    monitor='testMonitor', color=[0,0,0], colorSpace='rgb',
+    blendMode='avg', useFBO=True, 
+    units='height')
 
-msg.draw()
-win.flip()
-core.wait(3)
+defaultKeyboard = keyboard.Keyboard()
+
+running = True
+
+while running:
+    if defaultKeyboard.getKeys(["escape"]):
+        running = False
+
 win.close()
+core.quit()
