15270722d9a1f30c018eed018a60e587440cbaae
Hafsteinn Ragnarsson <haffi921@gmail.com>
2021-08-07 11:09:26 +0200

    Window settings now have defaults They can be overwritten by user, one by one

--- conflict_task/tests/manual/screens_test.py
+++ conflict_task/tests/manual/screens_test.py
@@ -4,37 +4,6 @@ from sys import path
 from conflict_task.devices import Window, Keyboard, DataHandler
 from conflict_task.experiment.sequence import Screen
 
-window_settings = dict(
-
-    # Size of the window in pixels (x, y)
-    size = (1920, 1200),
-
-    # Color of background as [r, g, b] list or single value. Each gun can take values between -1.0 and 1.0
-    color = [0, 0, 0],
-
-    # Create a window in ‘full-screen’ mode. Better timing can be achieved in full-screen mode
-    fullscreen = True,
-
-    # If not fullscreen, then position of the top-left corner of the window on the screen (x, y)
-    pos = (0, 0),
-
-    # Which monitor? 0 is primary monitor, 1 is secondary, etc.
-    screen = 0,
-
-    #---- Rest are less useful settings to tinker with, so it's not recommended ----#
-    monitor='testMonitor',
-
-    # Set the window type or back-end to use
-    winType = 'pyglet',
-
-    # Use framebuffer object
-    useFBO = True,
-
-    # Defines the default units of stimuli drawn in the window (can be overridden by each stimulus).
-    # Values can be None, ‘height’ (of the window), ‘norm’ (normalised), ‘deg’, ‘cm’, ‘pix’
-    units = 'height'
-)
-
 text = """
 Welcome to this test experiment. Right now you are looking at a Static Information Screen. \
 This is to provide you with some information about this experiment and what you should know before starting.
@@ -69,7 +38,7 @@ componentSettings = dict(
 
 data_handler = DataHandler("ScreenTest")
 input_device = Keyboard()
-window = Window(window_settings)
+window = Window()
 
 screen = Screen(window, input_device, data_handler, componentSettings)
 
